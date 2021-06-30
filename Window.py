548ca7413b680048edb777ff20eba3cbd82fd932
中西　洋翔 <aibou@nakanishiyoushounoiMac.local>
2021-06-30 14:33:17 +0900

    フレーム切り替え完了

--- Window.py
+++ Window.py
@@ -1,25 +1,94 @@
-import tkinter
+import tkinter as tk
+import menuFrame
+import settingFrame
+import gameFrame
 
-class WindowClass:
+class WindowClass(tk.Tk):
+
+	global MenuFrame, SettingFrame, GameFrame
+	WIDTH = 0
+	HEIGHT = 0
+
 	def __init__(self):
-		self.root = tkinter.Tk()
-		self.root.title('○×ゲーム')
-		self.root.geometry('250x160')
+		tk.Tk.__init__(self)
+		self.title('○×ゲーム')
+		self._frame = None
 
 		# メニューバーを作成
-		menubar = tkinter.Menu(self.root)
-		self.root.config(menu = menubar)
+		menubar = tk.Menu(self)
+		self.config(menu = menubar)
 
 		# メニューバーにメニューを作成
-		menu = tkinter.Menu(menubar)
+		menu = tk.Menu(menubar)
 		menubar.add_cascade(label = 'メニュー', menu = menu)
 
-		menu.add_command(label='メインメニュー')
-		menu.add_command(label='ゲーム開始')
-		menu.add_command(label='設定画面')
-		menu.add_command(label='ゲーム終了', command = self.root.destroy)
+		# メニューに子メニュー作成
+		menu.add_command(label='メインメニュー', command = lambda: self.show_frame('メニューフレーム'))
+		menu.add_command(label='ゲーム開始', command = lambda: self.show_frame('ゲームフレーム'))
+		menu.add_command(label='設定画面', command = lambda: self.show_frame('設定フレーム'))
+		menu.add_separator()
+		menu.add_command(label='ゲーム終了', command = self.destroy)
 
-		self.root.mainloop()
+		self.container = tk.Frame(self)
+		self.container.pack()
+
+		# 各フレームのインスタンス生成
+		global MenuFrame, SettingFrame, GameFrame
+		MenuFrame = menuFrame.MenuFrameClass(self.container, self)
+		SettingFrame = settingFrame.SettingFrameClass(self.container, self)
+		GameFrame = gameFrame.GameFrameClass(self.container, self)
+
+		self.show_frame('メニューフレーム')
+
+		self.resizable(False, False)
+		self.mainloop()
+
+	def getContainer(self):
+		return self._frame
+
+	def setContainer(self, targetFrame):
+		self._frame = targetFrame
+
+	def setWidth(self, width):
+		self.WIDTH = width
+
+	def setHeight(self, height):
+		self.HEIGHT = height
+
+	def getWidth(self):
+		return self.WIDTH
+
+	def getHeight(self):
+		return self.HEIGHT
+
+	def show_frame(self, targetFrame):
+		global MenuFrame, SettingFrame, GameFrame
+		for F in (MenuFrame, SettingFrame, GameFrame):
+			F.pack_forget()
+			
+		if targetFrame == 'メニューフレーム':
+			self.setContainer(MenuFrame)
+			self.setWidth(self.getContainer().getWidth())
+			self.setHeight(self.getContainer().getHeight())
+			MenuFrame.pack()
+
+		elif targetFrame == '設定フレーム':
+			self.setContainer(SettingFrame)
+			self.setWidth(self.getContainer().getWidth())
+			self.setHeight(self.getContainer().getHeight())
+			SettingFrame.pack()
+
+		elif targetFrame == 'ゲームフレーム':
+			self.setContainer(GameFrame)
+			self.setWidth(self.getContainer().getWidth())
+			self.setHeight(self.getContainer().getHeight())
+			GameFrame.pack()
+
+		# ウィンドウを中央に表示
+		self.update_idletasks()
+		x = (self.winfo_screenwidth() // 2) - (self.getWidth() // 2)
+		y = (self.winfo_screenheight() // 2) - (self.getHeight() // 2)
+		self.geometry('{}x{}+{}+{}'.format(self.getWidth(), self.getHeight(), x, y))
 
 if __name__ == '__main__':
 	wc = WindowClass()
---
+++ settingFrame.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+
+class SettingFrameClass(tk.Frame):
+
+	WIDTH = 500
+	HEIGHT = 300
+
+	def __init__(self, parent, controller):
+		tk.Frame.__init__(self, parent, width = self.WIDTH, height = self.HEIGHT)
+		self.pack_propagate(0)
+		tk.Label(self, text="設定フレーム", font=('Helvetica', 18, "bold")).pack()
+		# self.pack()
+
+	def getWidth(self):
+		return self.WIDTH
+
+	def getHeight(self):
+		return self.HEIGHT
